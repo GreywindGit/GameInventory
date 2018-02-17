cfbd20f0fd52e23c2d5d849e99fc829cb0b31e69
GreywindGit <alassea.indilwen@gmail.com>
2018-02-17 02:03:04 +0100

    Add add_to_inventory function

--- game_inv.rb
+++ game_inv.rb
@@ -4,11 +4,27 @@ def display_inventory(inventory)
     else
         puts "Inventory:"
         inventory.each {|key, value| puts "#{value} #{key}"}
-        puts "Total number of items: #{inventory.values.sum}"
+        puts "Total number of items: #{inventory.values.sum}\r\n"
     end
 end
 
+
+def add_to_inventory(inventory, added_items)
+    for item in added_items
+        if inventory.key?(item)
+            inventory[item] += 1
+        else
+            inventory[item] = 1
+        end
+    end
+end
+
+
 if __FILE__ == $0
-    inv = {"apple": 3, "gold coins": 25}
+    inv = {"apple" => 3, "gold coins" => 25}
     display_inventory(inv)
+    add_to_inventory(inv, ["gold coins", "torch", "chalk"])
+    display_inventory(inv)
+    add_to_inventory(inv, ["apple", "apple", "rope"])
+    print_inventory(inv)
 end
