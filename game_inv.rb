acb93617a26cedc055e2c275bb8b64377c0f2a1f
GreywindGit <alassea.indilwen@gmail.com>
2018-02-17 02:03:31 +0100

    Add prinv_inventory function

--- game_inv.rb
+++ game_inv.rb
@@ -20,6 +20,22 @@ def add_to_inventory(inventory, added_items)
 end
 
 
+def print_inventory(inventory)
+    amount_offset = inventory.values.map {|x| x.to_s.length}.max + 5
+    item_offset = inventory.keys.map {|x| x.length}.max + 5
+    puts "Inventory:"
+    puts " " * (amount_offset - "count".length) + "count" + 
+         " " * (item_offset - "item name".length) + "item name"
+    puts "-" * (amount_offset + item_offset)
+    for key, value in inventory
+        puts " " * (amount_offset - value.to_s.length) + "#{value}" +
+             " " * (item_offset - key.length) + "#{key}"
+    end
+    puts "-" * (amount_offset + item_offset)
+    puts "Total number of items: #{inventory.values.sum}"
+end
+
+
 if __FILE__ == $0
     inv = {"apple" => 3, "gold coins" => 25}
     display_inventory(inv)
